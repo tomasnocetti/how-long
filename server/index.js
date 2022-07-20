2be50864dc74817d4d18cce8a4e9c92820594acf
Tomas Nocetti <tnocetti@fi.uba.ar>
2022-07-19 21:05:13 -0300

    feat: add helmet middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const eventsRouter = require("./src/routes/events");
 
 const port = 3000;
+
+app.use(helmet());
 
 app.get("/", function (req, res, next) {
   res.send("Hello world!");
